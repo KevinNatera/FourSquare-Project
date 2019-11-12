f00710e7cbf0c0466a68e63a4a2c681dd7d7406e
Kevin Natera <mycityneedsme@Kevins-MacBook-Pro-2.local>
2019-11-12 17:02:14 -0500

    Initial Commit

---
+++ FourSquare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FourSquare
+//
+//  Created by Kevin Natera on 11/12/19.
+//  Copyright © 2019 Kevin Natera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
