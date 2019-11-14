23d2619b8887a09f0a1a742b868b5d9094959850
Kevin Natera <mycityneedsme@Kevins-MacBook-Pro-2.local>
2019-11-14 11:27:08 -0500

    Created Venue Model

--- FourSquare/Models/VenueModel.swift
+++ FourSquare/Models/VenueModel.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import MapKit
+import CoreLocation
+
+struct VenueResponse: Codable {
+    let response: VenueWrapper
+}
+struct VenueWrapper: Codable {
+    let venues: [Venue]
+}
+
+class Venue: NSObject, Codable, MKAnnotation {
+    let name: String
+    let location: LocationWrapper
+    
+    @objc var coordinate: CLLocationCoordinate2D {
+        let latLong = [location.lat, location.lng]
+        return CLLocationCoordinate2D(latitude: latLong[0], longitude: latLong[1])
+    }
+    
+    var hasValidCoordinates: Bool {
+        return coordinate.latitude != 0 && coordinate.longitude != 0
+    }
+    
+    static func getVenues(from data: Data) throws -> [Venue]? {
+        do {
+            let response = try JSONDecoder().decode(VenueResponse.self,from: data)
+            return response.response.venues
+        } catch {
+            return nil
+        }
+    }
+}
+
+struct LocationWrapper: Codable {
+    let address: String?
+    let lat: Double
+    let lng: Double
+}
