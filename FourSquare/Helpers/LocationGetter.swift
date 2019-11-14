fe23d9020aaae29e32412acf64662b93eac9895a
Kevin Natera <mycityneedsme@Kevins-MacBook-Pro-2.local>
2019-11-13 21:27:37 -0500

    Added gitignore

---
+++ FourSquare/Helpers/LocationGetter.swift
@@ -0,0 +1,9 @@
+//
+//  LocationGetter.swift
+//  FourSquare
+//
+//  Created by Kevin Natera on 11/13/19.
+//  Copyright © 2019 Kevin Natera. All rights reserved.
+//
+
+import Foundation
---
+++ FourSquare/Helpers/NetworkHelper.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  FourSquare
+//
+//  Created by Kevin Natera on 11/13/19.
+//  Copyright © 2019 Kevin Natera. All rights reserved.
+//
+
+import Foundation
